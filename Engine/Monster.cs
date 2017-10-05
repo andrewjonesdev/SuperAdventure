8bf3575a3e29472ab4e2066c61837a45fb69a000
Andrew Jones <jones.gaffney1920@gmail.com>
2017-10-05 11:10:35 -0400

    Added SuperClasses/Inheritance

--- Engine/Monster.cs
+++ Engine/Monster.cs
@@ -1,11 +1,9 @@
 ﻿namespace Engine
 {
-    public class Monster
+    public class Monster : LivingCreature
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int MaximumHitPoints { get; set; }
-        public int CurrentHitPoints { get; set; }
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
     }
